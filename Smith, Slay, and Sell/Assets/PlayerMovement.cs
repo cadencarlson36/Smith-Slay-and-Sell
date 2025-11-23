b4b191e334f49135a76490a0751affcc56c79604
Nicholas Coiner <127805001+npcoiner@users.noreply.github.com>
2025-11-23 01:21:55 -0800

    Implement player movement functionality

---
+++ Smith, Slay, and Sell/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+	private float playerSpeed = 5.0f;
+
+	private CharacterController controller;
+	private Vector3 playerVelocity;
+
+	[Header("Input Actions")]
+	// In the editor select the Input Action Reference for movement
+	public InputActionReference moveAction; 
+	private void Awake(){
+		//This if statement attemps to get a component if it already exists,
+		//otherwise it adds it. This saves editor memory and allows easier modularity.
+		if (!TryGetComponent<CharacterController>(out controller)){
+			controller = gameObject.AddComponent<CharacterController>();
+		}
+		controller.minMoveDistance = 0f;
+		//Debug.Log(controller);
+	}
+
+	void Update(){
+
+		Vector2 input = moveAction.action.ReadValue<Vector2>();
+		Vector3 move = new Vector3(input.x, 0, input.y);
+		move = move.normalized;
+
+		if (move != Vector3.zero){
+		    transform.forward = move;
+		}
+		Vector3 finalMove = (move * playerSpeed) ;
+		//Debug.Log(finalMove);
+		controller.Move(finalMove * Time.deltaTime);
+	}
+}
