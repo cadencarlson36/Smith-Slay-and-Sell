fd054fe64a724c7ab0089649adced7dca47a7481
npcoiner <nicholascoiner@gmail.com>
2026-01-06 14:33:30 -0800

    Implemented player interaction sphere

--- Smith, Slay, and Sell/Assets/PlayerInteract.cs
+++ Smith, Slay, and Sell/Assets/PlayerInteract.cs
@@ -1,36 +1,50 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine; 
+using UnityEngine.InputSystem; 
 
-public class interact : MonoBehaviour
-{
+public class Interact : MonoBehaviour { 
+	private CharacterController controller; 
+	[Header("Input Actions")] 
+		public InputActionReference interactAction; 
+	[Header("Debug Options")] 
+		public bool showInteractSphere = false; 
 
-	private CharacterController controller;
+	private Transform interactSphereTransform; 
 
-	[Header("Input Actions")]
+	private void Awake() { 
+		if (!TryGetComponent<CharacterController>(out controller)) 
+			controller = gameObject.AddComponent<CharacterController>(); 
 
-	public InputActionReference interactAction; 
+		interactSphereTransform = transform.Find("InteractSphere"); 
+		if (!interactSphereTransform) { 
+			var interactSphereObj = new GameObject("InteractSphere"); 
+			interactSphereObj.transform.SetParent(transform); 
+			interactSphereObj.transform.localPosition = new Vector3(0, 0, 1); 
+			interactSphereObj.AddComponent<SphereCollider>().isTrigger = true; 
+			interactSphereTransform = interactSphereObj.transform; 
+			var meshFilter = interactSphereObj.GetComponent<MeshFilter>(); 
+			var meshRenderer = interactSphereObj.GetComponent<MeshRenderer>(); 
+			meshFilter = interactSphereObj.AddComponent<MeshFilter>(); 
+			meshFilter.mesh = Resources.GetBuiltinResource<Mesh>("Sphere.fbx"); 
+			meshRenderer = interactSphereObj.AddComponent<MeshRenderer>(); 
+			meshRenderer.material.color = Color.green; 
+		} 
+	} 
 
-	private void Awake(){
-		//This if statement attemps to get a component if it already exists,
-		//otherwise it adds it. This saves editor memory and allows easier modularity.
-		if (!TryGetComponent<CharacterController>(out controller)){
-			controller = gameObject.AddComponent<CharacterController>();
+	private void OnValidate() { 
+		if (!interactSphereTransform) return; 
+		var meshRenderer = interactSphereTransform.GetComponent<MeshRenderer>();
+		if (meshRenderer){
+			meshRenderer.enabled = showInteractSphere;
 		}
-		//Debug.Log(controller);
-	}
 
-	void Update(){
-		interactAction.action.started += 
-			context => {
-				Debug.Log("Player interact");
-			};
-		interactAction.action.performed += 
-			context => {
-				Debug.Log("Player interact held");
-			};
-		interactAction.action.canceled += 
-			context => {
-				Debug.Log("Player interact canceled");
-			};
-	}
+	} 
+	void Update() { 
+		// Not best practice to subscribe every frame, put this in Start or Awake for real code! 
+		interactAction.action.started += ctx => 
+			Debug.Log("Interact started"); 
+		interactAction.action.performed += ctx => 
+			Debug.Log("Interact held"); 
+		interactAction.action.canceled += ctx => 
+			Debug.Log("Interact canceled"); 
+	} 
 }
