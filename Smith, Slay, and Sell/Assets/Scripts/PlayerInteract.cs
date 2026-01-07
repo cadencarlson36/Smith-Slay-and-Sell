75f7ebc196f60271390da0b55c51184ece003685
npcoiner <nicholascoiner@gmail.com>
2026-01-07 13:36:47 -0800

    Added comments for clarification

--- Smith, Slay, and Sell/Assets/Scripts/PlayerInteract.cs
+++ Smith, Slay, and Sell/Assets/Scripts/PlayerInteract.cs
@@ -1,57 +1,75 @@
-using UnityEngine; 
-using UnityEngine.InputSystem; 
+using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Interact : MonoBehaviour { 
-	private CharacterController controller; 
-	[Header("Debug Options")] 
-		public bool showInteractSphere = false; 
 
-	private Transform interactSphereTransform; 
+public class Interact : MonoBehaviour
+{
+    private CharacterController controller;
+    [Header("Debug Options")]
+    public bool showInteractSphere = false;
 
-	private void Awake() { 
-		if (!TryGetComponent<CharacterController>(out controller)) 
-			controller = gameObject.AddComponent<CharacterController>(); 
+    private Transform interactSphereTransform;
 
-		interactSphereTransform = transform.Find("InteractSphere"); 
-		if (!interactSphereTransform) { 
-			var interactSphereObj = new GameObject("InteractSphere"); 
-			interactSphereObj.transform.SetParent(transform); 
-			interactSphereObj.transform.localPosition = new Vector3(0, 0, 1); 
-			interactSphereObj.AddComponent<SphereCollider>().isTrigger = true; 
-			interactSphereTransform = interactSphereObj.transform; 
-			var meshFilter = interactSphereObj.GetComponent<MeshFilter>(); 
-			var meshRenderer = interactSphereObj.GetComponent<MeshRenderer>(); 
-			meshFilter = interactSphereObj.AddComponent<MeshFilter>(); 
-			meshFilter.mesh = Resources.GetBuiltinResource<Mesh>("Sphere.fbx"); 
-			meshRenderer = interactSphereObj.AddComponent<MeshRenderer>(); 
-			meshRenderer.material.color = Color.green; 
-			meshRenderer.enabled = showInteractSphere;	
-		} 
-		var interactAction = GetComponent<PlayerInput>().actions["Interact"];
-		interactAction.started += OnInteractStarted;
-		interactAction.performed += OnInteractPerformed;
-		interactAction.canceled += OnInteractCanceled;
-	} 
+    private void Awake()
+    {
+        //This code attempts to get a component on the attatched game object and otherwise creates it.
+        //This ensures that the required component is available without having to add it manually.
+        if (!TryGetComponent<CharacterController>(out controller))
+            controller = gameObject.AddComponent<CharacterController>();
 
-	private void OnValidate() { 
-		if (!interactSphereTransform) return; 
-		var meshRenderer = interactSphereTransform.GetComponent<MeshRenderer>();
-		if (meshRenderer){
-			meshRenderer.enabled = showInteractSphere;
-		}
-	} 
-	private void OnInteractStarted(InputAction.CallbackContext ctx){
-			Debug.Log("Interact started"); 
-	}
+        //This code searches for the InteractSphere, and if it doesn't exist, it creates a new one.
+        //This is slightly unnecessary to be completely honest.
+        interactSphereTransform = transform.Find("InteractSphere");
+        if (!interactSphereTransform)
+        {
+            var interactSphereObj = new GameObject("InteractSphere");
+            interactSphereObj.transform.SetParent(transform);
+            interactSphereObj.transform.localPosition = new Vector3(0, 0, 1);
+            interactSphereObj.AddComponent<SphereCollider>().isTrigger = true;
+            interactSphereTransform = interactSphereObj.transform;
+            var meshFilter = interactSphereObj.GetComponent<MeshFilter>();
+            var meshRenderer = interactSphereObj.GetComponent<MeshRenderer>();
+            meshFilter = interactSphereObj.AddComponent<MeshFilter>();
+            meshFilter.mesh = Resources.GetBuiltinResource<Mesh>("Sphere.fbx");
+            meshRenderer = interactSphereObj.AddComponent<MeshRenderer>();
+            meshRenderer.material.color = Color.green;
+            meshRenderer.enabled = showInteractSphere;
+        }
+        //Set callback functions
+        var interactAction = GetComponent<PlayerInput>().actions["Interact"];
+        interactAction.started += OnInteractStarted;
+        interactAction.performed += OnInteractPerformed;
+        interactAction.canceled += OnInteractCanceled;
+    }
 
-	private void OnInteractPerformed(InputAction.CallbackContext ctx){
+    //OnValidate runs on both awake and on editor change to show the interactSphere for debugging purposes.
+    private void OnValidate()
+    {
+        if (!interactSphereTransform) return;
+        var meshRenderer = interactSphereTransform.GetComponent<MeshRenderer>();
+        if (meshRenderer)
+        {
+            meshRenderer.enabled = showInteractSphere;
+        }
+    }
 
-			Debug.Log("Interact held"); 
-	}
-	private void OnInteractCanceled(InputAction.CallbackContext ctx){
+    //Callback function definitions for each of the interactions
+    private void OnInteractStarted(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Interact started");
+    }
 
-			Debug.Log("Interact canceled"); 
-	}
-	void Update() { 
-	} 
+    private void OnInteractPerformed(InputAction.CallbackContext ctx)
+    {
+
+        Debug.Log("Interact held");
+    }
+    private void OnInteractCanceled(InputAction.CallbackContext ctx)
+    {
+
+        Debug.Log("Interact canceled");
+    }
+    void Update()
+    {
+    }
 }
--- Smith, Slay, and Sell/Assets/Scripts/PlayerMovement.cs
+++ Smith, Slay, and Sell/Assets/Scripts/PlayerMovement.cs
@@ -3,41 +3,46 @@ using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-	private float playerSpeed = 5.0f;
+    private float playerSpeed = 5.0f;
 
-	private CharacterController controller;
-	private Vector3 playerVelocity;
-	private float deadzone = 0.2f;
-	private float turnSmoothSpeed = 60f;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private float deadzone = 0.2f;
+    private float turnSmoothSpeed = 60f;
 
-	private PlayerInput playerInput;
+    private PlayerInput playerInput;
 
-	private void Start(){
-		//This if statement attemps to get a component if it already exists,
-		//otherwise it adds it. This saves editor memory and allows easier modularity.
-		if (!TryGetComponent<CharacterController>(out controller)){
-			controller = gameObject.AddComponent<CharacterController>();
-		}
-		controller.minMoveDistance = 0f;
-		playerInput = GetComponent<PlayerInput>();
-		//Debug.Log(controller);
-	}
+    private void Start()
+    {
+        //This if statement attemps to get a component if it already exists,
+        //otherwise it adds it. This saves editor memory and allows easier modularity.
+        if (!TryGetComponent<CharacterController>(out controller))
+        {
+            controller = gameObject.AddComponent<CharacterController>();
+        }
+        controller.minMoveDistance = 0f;
+        playerInput = GetComponent<PlayerInput>();
+        //Debug.Log(controller);
+    }
 
-	void Update(){
+    void Update()
+    {
 
-		Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
-		if (input.magnitude < deadzone){
-			return;
-		}
-		Vector3 move = new Vector3(input.x, 0, input.y);
-		move = move.normalized;
+        Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
+        if (input.magnitude < deadzone)
+        {
+            return;
+        }
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = move.normalized;
 
-		if (move != Vector3.zero){
-			Quaternion targetRotation = Quaternion.LookRotation(move, Vector3.up);
-			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSmoothSpeed * Time.deltaTime);
-		}
-		Vector3 finalMove = (move * playerSpeed) ;
-		//Debug.Log(finalMove);
-		controller.Move(finalMove * Time.deltaTime);
-	}
+        if (move != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSmoothSpeed * Time.deltaTime);
+        }
+        Vector3 finalMove = (move * playerSpeed);
+        //Debug.Log(finalMove);
+        controller.Move(finalMove * Time.deltaTime);
+    }
 }
