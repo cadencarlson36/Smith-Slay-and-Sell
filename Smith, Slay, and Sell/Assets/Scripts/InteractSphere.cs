1cdd2985975b18fcb61ca230c9d7a5c47060b4d9
npcoiner <nicholascoiner@gmail.com>
2026-01-11 18:20:59 -0800

    Added clarifying comments to InteractSphere.cs

--- Smith, Slay, and Sell/Assets/Scripts/InteractSphere.cs
+++ Smith, Slay, and Sell/Assets/Scripts/InteractSphere.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 
 
 // TODO filter for pickupable/interactables probably using tags.
+// This script is used in the InteractSphere object created in PlayerInteract.cs
+// It currently provides public functions to get objects with colliders that the sphere
+// intersects with.
 public class InteractSphere : MonoBehaviour
 {
     private List<GameObject> objectsInRange = new List<GameObject>();
+
     void Update()
     {
         foreach (var obj in objectsInRange)
@@ -13,6 +17,9 @@ public class InteractSphere : MonoBehaviour
             //Debug.Log($"In range: {obj.name}");
         }
     }
+    //This function returns the first object in the list.
+    //DISCLAIMER: This is NOT the nearest item in range as of yet.
+    //This function will need to be updated with logic to get the closest object relative to the player.
     //TODO make this actually return the nearest and not just the first in the list
     public GameObject GetNearestInRange()
     {
@@ -20,14 +27,14 @@ public class InteractSphere : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        //Debug.Log($"Enter: {other.name}");
+        //When a collider enters the sphere, we add the object to the objectsInRange list.
         objectsInRange.Add(other.gameObject);
     }
 
     void OnTriggerExit(Collider other)
     {
-        //Debug.Log($"Exit: {other.name}");
 
+        //When a collider exits the sphere, we remove the object from the objectsInRange list.
         objectsInRange.Remove(other.gameObject);
     }
 }
